de985ad0900e72088463b74d20ac767f1bdad26f
zj13600231175 <webday2018@163.com>
2020-09-09 21:05:32 +0800

    初始化代码

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+// 引入 vant 组件
+import Vant from 'vant'
+// 2. 引入组件 css 文件
+import 'vant/lib/index.css'
+// 3. 注册 vant ui 组件库的标签
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
+  axios,
+  render: (h) => h(App),
 }).$mount('#app')
